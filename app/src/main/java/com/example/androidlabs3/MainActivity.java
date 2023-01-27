76cbd054d0ea9c771704ded7ccfe498ffd251ec1
Tazwar <afta0003@algonquinlive.com>
2023-01-26 23:09:29 -0500

    lab 2

--- app/src/main/java/com/example/androidlabs3/MainActivity.java
+++ app/src/main/java/com/example/androidlabs3/MainActivity.java
@@ -1,14 +1,39 @@
-package com.example.androidlabs3;
+package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView firstText = findViewById(R.id.inputtext);
+
+        EditText theEdit = findViewById(R.id.editText);
+
+        final Button btn = findViewById(R.id.button);
+        btn.setText("Press Me");
+        btn.setOnClickListener( (click) ->  { btn.setText("Label has been updated"); });
+
+        CheckBox cb = findViewById(R.id.checkBox);
+        cb.setOnCheckedChangeListener( (compoundButton, b) -> {
+
+            Snackbar.make(theEdit, "Checkbox is now " + b, Snackbar.LENGTH_LONG)
+                    .setAction("Undo", click-> compoundButton.setChecked( !b )).show();
+
+        });
     }
 }
